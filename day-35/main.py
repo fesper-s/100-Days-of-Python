a4f8611060767e4b4414856ae7e494639c7c10ef
fesper-s <fesper-s@student.42.rio>
2023-11-23 17:45:01 -0300

    updatind day-35

---
+++ day-35/main.py
@@ -0,0 +1,27 @@
+import requests
+
+OWM_Endpoint = "https://api.openweathermap.org/data/3.0/"
+
+api_key = "87cdfb400a64f1f19082ae1f84c8ece1"
+
+weather_params = {
+    "lat": -22.906847,
+    "lon": -43.172897,
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella.")
